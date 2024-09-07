17cad0e1039e7da123befd5290647e848e021572
yangyang <2030819784@qq.com>
2024-09-07 18:23:18 +0800

    feat: 配置pinia

---
+++ src/pages/index/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <view>
+    {{store.token}}
+  </view>
+</template>
+
+<script setup lang="ts">
+import { useLoginStore } from '@/store'
+const store = useLoginStore()
+</script>
+
+<style lang="scss"></style>
