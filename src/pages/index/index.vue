15f6dc9d304fd1f72873fa5952f4723bb826a663
yangyang <2030819784@qq.com>
2024-09-15 13:27:33 +0800

    feat:解决微信小程序导入组件报错

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,80 +1,16 @@
 <script setup lang="ts">
-import { onLoad, onShow } from '@dcloudio/uni-app'
-import { ref } from 'vue'
-import PageNavbar from './components/PageNavbar.vue'
-import CategoryPanel from '/components/CategoryPanel.vue'
-import Carousel from './components/Carousel.vue'
-import CommodityClassify from './components/CommodityClassify.vue'
-
-
-// 是否加载中标记
-const isLoading = ref(false)
-
-const categoryList = ref([
-  {
-    key: '1',
-    name: '火锅',
-  },
-  {
-    key: '2',
-    name: '麻辣烫',
-  },
-  {
-    key: '3',
-    name: '烤肉',
-  },
-  {
-    key: '4',
-    name: '面馆',
-  },
-  {
-    key: '5',
-    name: '肉夹馍',
-  },
-])
-// 页面加载
-onLoad(async () => {
-  isLoading.value = true
-  // await 
-  isLoading.value = false
-})
-
-// 当前下拉刷新状态
-const isTriggered = ref(false)
-// 自定义下拉刷新被触发
-const onRefresherrefresh = async () => {
-  // 开始动画
-  isTriggered.value = true
-  // 加载数据
-  // await getHomeCategoryData()
-  // 关闭动画
-  isTriggered.value = false
-}
 </script>
 
 <template>
-  <!-- 滚动容器 -->
-  <scroll-view enable-back-to-top refresher-enabled @refresherrefresh="onRefresherrefresh"
-    :refresher-triggered="isTriggered" class="scroll-view" scroll-y>
-    <view class="top">
-      <!-- 自定义导航栏 -->
-      <PageNavbar />
-      <!-- 轮播图-->
-      <view class="carousel">
-        <Carousel></Carousel>
-      </view>
-    </view>
-    <view class="categoryPanel">
-      <!-- 分类面板 -->
-      <CategoryPanel :list="categoryList" />
-      <!-- 商品分类 -->
-      <CommodityClassify></CommodityClassify>
-    </view>
-  </scroll-view>
+  <view >
+    <text >
+      1232
+    </text>
+  </view>
 </template>
 
 <style lang="scss" scoped>
-page {
+.page {
   display: flex;
   flex-direction: column;
   height: 100%;
